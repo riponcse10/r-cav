7af1e919825f74746f9050af499d9c8d3ddb150d
Ripon <mahoque@uab.edu>
2021-02-07 21:55:49 -0600

    Add Yolov3 to perform object detection

--- network/client.py
+++ network/client.py
@@ -1,14 +1,31 @@
 
+import socket
+
+HOST = '127.0.0.1'  # The server's hostname or IP address
+PORT = 65432        # The port used by the server
+DATA_PATH = "../../kitti-data/training/image_2/000010.png"
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+	s.connect((HOST, PORT))
+	#s.sendall(b'Hello, world')
+	img = open(DATA_PATH,'rb')
+	while True:
+	    strng = img.read(1024)
+	    if not strng:
+	        break
+	    s.send(strng)
+	img.close()
+
+#print('Received', repr(data))
 
 
+#class Client():
+#    def __init__(self, service_client, host, port):
+#        reactor.connectTCP(cfg.SERVER_HOST, cfg.SERVER_PORT,
+#                           ClientProtocolFactory(service_client))
 
-class Client():
-    def __init__(self, service_client, host, port):
-        reactor.connectTCP(cfg.SERVER_HOST, cfg.SERVER_PORT,
-                           ClientProtocolFactory(service_client))
+#    def run(self):
+#        reactor.run()
 
-    def run(self):
-        reactor.run()
-
-    def stop(self):
-        reactor.callFromThread(reactor.stop)
+#    def stop(self):
+#       reactor.callFromThread(reactor.stop)
--- network/server.py
+++ network/server.py
@@ -1,2 +1,37 @@
-class Server:
-    def __init__(self):
+import socket
+from yolo_model import YOLOv3
+import time
+
+HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
+PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
+
+yolo = YOLOv3()
+model = yolo.load()
+q = []
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+	s.bind((HOST, PORT))
+	s.listen()
+	conn, addr = s.accept()
+	print('Connected by', addr)
+	start_time = time.time()
+	with conn:
+		fname = '000.png'
+		#data = s.recv(1024)
+		fp = open(fname,'wb')
+		while True:
+			strng = conn.recv(1024)
+			if not strng:
+				break
+			fp.write(strng)
+    
+		fp.close()
+		q.append(fname)
+		
+		yolo.calculate(model, q.pop(0))
+		
+	end_time = time.time()
+	print(end_time-start_time)
+
+#class Server:
+#    def __init__(self):
